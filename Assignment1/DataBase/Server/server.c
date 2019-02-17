765cfd7b50f4ba20ba0c2a72138505a5c70e9ee7
ayushi712 <47599099+ayushi712@users.noreply.github.com>
2019-02-17 21:50:04 +0530

    Create server.c

---
+++ Assignment1/DataBase/Server/server.c
@@ -0,0 +1,99 @@
+#include <stdio.h> 
+#include <stdlib.h>
+#include <sys/ipc.h> 
+#include <sys/msg.h> 
+#include<time.h>
+#include<string.h>
+
+  
+char*data,c;
+FILE*fp;
+
+// structure for message queue
+struct mesg_buffer 
+{ 
+    long mesg_type; 
+    char mesg_text[100]; 
+}filename; 
+
+	void append(char* s, char c)
+	{
+        	int len = strlen(s);
+        	s[len] = c;
+        	s[len+1] = '\0';
+	}
+  
+int main() 
+{ 
+    key_t key; 
+    int msgid;
+    clock_t start, end;
+    double cpu_time_used; 
+  
+    // ftok to generate unique key 
+    key = ftok("progfile", 65); 
+  
+    // msgget creates a message queue and returns identifier 
+    msgid = msgget(key, 0666 | IPC_CREAT); 
+
+     //clock for timer 
+    start = clock();
+    
+    while(1)
+	{                        //while loop starts
+    
+    
+    // msgrcv to receive filename 
+    msgrcv(msgid, &filename, sizeof(filename), 1, 0);
+    data = filename.mesg_text; 
+      
+    msgid = msgget(key, 0666 | IPC_CREAT); 
+    filename.mesg_type = 1;
+    
+    //open file
+    fp = fopen(filename.mesg_text, "r");
+    
+    if (fp==NULL)
+    {
+     printf("Error while opening the file.\n");
+     exit(0);    //exit
+    }
+
+    //read contents from file
+    c=fgetc(fp);
+    while(c!=EOF)
+    {
+     printf("%c",c);
+     append(data, c);
+     c=fgetc(fp);
+    }
+
+    fclose(fp);
+    printf("%s",data);
+   
+        end = clock();
+	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+	printf("Time is: %f\n",cpu_time_used); 
+
+
+    // msgsnd to send message 
+    msgsnd(msgid, &filename, sizeof(filename), 0); 
+    
+    //fclose(fp);
+
+	 printf("Press c and enter to continue, ~ and enter to quit" );
+        if(getchar()=='~')
+		{
+	  	break;	
+		}
+	 
+	
+ 	}                        //while loop ends
+ 
+
+    // to destroy the message queue 
+    msgctl(msgid, IPC_RMID, NULL); 
+  
+    
+    return 0; 
+}
